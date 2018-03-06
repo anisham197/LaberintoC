38a1e76d8cb104faef4d289c75e969c57a37fdb4
anisham197 <anisham197@gmail.com>
2018-03-06 12:10:29 +0530

    Location Recycler View

--- app/src/main/java/in/goflo/laberintoc/View/Activity/MainActivity.java
+++ app/src/main/java/in/goflo/laberintoc/View/Activity/MainActivity.java
@@ -1,15 +1,36 @@
 package in.goflo.laberintoc.View.Activity;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import in.goflo.laberintoc.Model.LocationDetails;
 import in.goflo.laberintoc.R;
+import in.goflo.laberintoc.View.Adapter.LocationListAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final private String TAG = "MainActivity";
+    RecyclerView recyclerView;
+    LocationListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        List<LocationDetails> locationDetails = new ArrayList<>();
+
+        adapter = new LocationListAdapter(getApplicationContext(), locationDetails);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
     }
 }
