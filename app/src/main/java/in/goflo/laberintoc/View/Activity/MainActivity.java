0e81623ea6f04412873a0402d504e56454160e06
amisha <amisha.agarwal29@gmail.com>
2018-03-06 12:12:02 +0530

    location retieval

--- app/src/main/java/in/goflo/laberintoc/View/Activity/MainActivity.java
+++ app/src/main/java/in/goflo/laberintoc/View/Activity/MainActivity.java
@@ -1,9 +1,16 @@
 package in.goflo.laberintoc.View.Activity;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 import in.goflo.laberintoc.R;
+import in.goflo.laberintoc.ViewModel.LocationViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocationViewModel locationViewModel =
+                ViewModelProviders.of(this).get(LocationViewModel.class);
+
+        LiveData<List<LocationDetails>> locationLiveData = locationViewModel.getLocationLiveData();
+        locationLiveData.observe(this, new Observer<List<LocationDetails>>() {
+            @Override
+            public void onChanged(@Nullable List<LocationDetails> locationDetails) {
+                if(locationDetails != null) {
+                }
+            }
+        });
+
     }
 }
