e91e954bc70555c3122176a67a2e0405440ac407
amisha <amisha.agarwal29@gmail.com>
2018-03-06 11:41:28 +0530

    Initial project structure

--- app/src/main/java/in/goflo/laberintoc/View/Activity/MainActivity.java
+++ app/src/main/java/in/goflo/laberintoc/View/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package in.goflo.laberintoc;
+package in.goflo.laberintoc.View.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import in.goflo.laberintoc.R;
 
 public class MainActivity extends AppCompatActivity {
 
